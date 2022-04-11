9b0f0696b7a6aaf87046393b78f1b54d2290bf2b
Anthony Aguilar <anthonyaguilar94@gmail.com>
2022-04-11 18:06:40 -0300

    empezando forEach para filtrar

---
+++ src/main/java/app/dto/IngredienteList.java
@@ -0,0 +1,32 @@
+package app.dto;
+
+public class IngredienteList {
+	
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private String amount;
+	
+	public IngredienteList() {}
+	
+	public IngredienteList(String name, String amount) {
+		this.name = name;
+		this.amount = amount;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+
+}
--- src/main/java/app/ingredient/service/IngredientServiceImpl.java
+++ src/main/java/app/ingredient/service/IngredientServiceImpl.java
@@ -7,33 +7,43 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import app.datasource.IngredientDataSource;
+import app.dto.Ingredients;
 import app.dto.IngredientsResponse;
 import app.recipe.entity.Ingredient;
 
-public class IngredientServiceImpl implements IngredientService{
-	
+public class IngredientServiceImpl implements IngredientService {
+
 	private static final Logger logger = LoggerFactory.getLogger(IngredientServiceImpl.class);
-		
+
 	@Autowired
 	private IngredientDataSource ingredientDatasource;
 
 	@Override
 	public IngredientsResponse getIngredients() {
-		
+
 		IngredientsResponse response = new IngredientsResponse();
 		response.setCode("200");
 		response.setDescription("ok");
-		
+
 		Iterable<Ingredient> ingredients = ingredientDatasource.findAll();
-		
-		ingredients.forEach( daoIngredient -> {
+
+		ingredients.forEach(daoIngredient1 -> {
+
+			ingredients.forEach(daoIngredient2 -> {
+
+				if (daoIngredient1.getName() == daoIngredient2.getName()) {
+					Double amount = daoIngredient1.getAmount() + daoIngredient2.getAmount();
+					daoIngredient1.setAmount(amount);
+				}
 			
-			Ingredient ingredient = (Ingredient) daoIngredient;
+			});
+
+			Ingredient ingredient = (Ingredient) daoIngredient1;
 			response.getIngredients().add(ingredient);
 		});
-		
+
 		return response;
-		
+
 	}
-	
+
 }
