92d68b9df436c5e9c990c3ceec4842f02ddae09c
Anthony Aguilar <anthonyaguilar94@gmail.com>
2022-04-12 03:00:22 -0300

    ingredientes no repetidos

--- src/main/java/app/datasource/IngredientDataSource.java
+++ src/main/java/app/datasource/IngredientDataSource.java
@@ -1,20 +1,16 @@
 package app.datasource;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import app.dto.IngredietItem;
 import app.recipe.entity.Ingredient;
 
 @Repository
 public interface IngredientDataSource extends JpaRepository<Ingredient, Serializable>{
-
-	List<Ingredient> findAll();
 
 	
 	@Query(value = "SELECT c.name, SUM(c.amount)\n"
